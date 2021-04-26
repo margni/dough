d929cebe07a69656f037776f62397607089c5e46
Steve I <1615137+jixor@users.noreply.github.com>
2021-04-26 21:46:19 +0100

    Cypress Test

---
+++ cypress/integration/simple-recipe.spec.js
@@ -0,0 +1,11 @@
+it('Simple Recipe', () => {
+  cy.visit('/');
+
+  cy.findByLabelText('Count').type('{backspace}4');
+  cy.findByLabelText('Weightg').type('{leftArrow}{backspace}6');
+
+  cy.findByLabelText('Flourg').should('have.value', '552');
+  cy.findByLabelText('Waterg').should('have.value', '295');
+  cy.findByLabelText('Starterg').should('have.value', '182');
+  cy.findByLabelText('Saltg').should('have.value', '11');
+});
